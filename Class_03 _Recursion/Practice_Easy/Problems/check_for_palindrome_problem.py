8bda3d5db1ebb40890dfb6f3f0bb884cc3a82eb5
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-10 20:21:58 -0700

    Class 3

--- Class_03 _Recursion/Practice_Easy/Problems/check_for_palindrome_problem.py
+++ Class_03 _Recursion/Practice_Easy/Problems/check_for_palindrome_problem.py
@@ -1,1 +1,22 @@
 # Write a function that checks if a string is a palindrome using recursion
+
+def checkPalindrom(string):
+    return string == reverse(string)
+
+def reverse(string):
+    if string == "":
+        return ""
+    else:
+        return (reverse(string[1:]) + string[0])
+
+
+def recursivePalindrome(string):
+    if len(string) == 1 or len(string) == 0:
+        return True
+    if string[0] == string[len(string) - 1]:
+        return recursivePalindrome(string[1:-1])
+    return False
+        
+
+print(checkPalindrom("racecar"))
+print(recursivePalindrome("racecar"))
