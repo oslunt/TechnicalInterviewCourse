569108d6a5b2ee4e7148a1d6535b9b5e21e08a6f
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-10 20:21:58 -0700

    Class 3

--- Class_03 _Recursion/Class_Example_Fibonacci_Sequence/Problems/find_fibonacci_sequence_iteration_problem.py
+++ Class_03 _Recursion/Class_Example_Fibonacci_Sequence/Problems/find_fibonacci_sequence_iteration_problem.py
@@ -1,1 +1,12 @@
 # Write a program that creates a portion of the fibonacci sequence iteratively
+
+def fibseq(n):
+    if (n == 1) or (n == 2):
+        return 1
+    else:
+        return fibseq(n-1) + fibseq(n-2)
+
+print(fibseq(7))
+
+# O(branches^depth)
+# so fibonacci sequence is O(2^n)
