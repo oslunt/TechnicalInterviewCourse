0de10c43f4b35cc3b8011a6da592a414824c5e4c
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-12 17:04:04 -0700

    Array HW

--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/contains_duplicates_problem.py
@@ -2,5 +2,21 @@
 # Your function should return false if every element is distinct.
 # This problem came from leetcode.com
 
+def bruteForce(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if i != j:
+                if lst[i] == lst[j]:
+                    return True
+    return False
+
+def optimized(lst):
+    no_dupes = set()
+    for i in range(len(lst)):
+        no_dupes.add(lst[i])  
+    return len(lst) != len(no_dupes)
+
 input_array = [1, 2, 3, 3, 4]
+print(bruteForce(input_array))
+print(optimized(input_array))
 # Output = True
