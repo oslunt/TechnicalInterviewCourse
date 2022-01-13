ee9c87e94e8e1cc7445137bfe6e5588cfa4baec0
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-12 17:04:04 -0700

    Array HW

--- Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
+++ Class_04_Array_Problems_Easy/Array_Problems_Easy_Homework/Problems/best_time_to_buy_stock_problem.py
@@ -3,5 +3,31 @@
 # sell one share of the stock) design an algorithm to find the maximum profit
 # This problem came from leetcode.com
 
+# brute force
+
+def bruteForce(lst):
+    max = 0
+    cur = 0
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            cur = lst[j] - lst[i]
+            if cur > max:
+                max = cur
+    return max
+
+def optimized(lst):
+    max = 0
+    min = lst[0]
+    for i in range(len(lst)):
+        if lst[i] < min:
+            min = lst[i]
+        else:
+            if (lst[i] - min) > max:
+                max = lst[i] - min
+    return max
+
+
 input_array = [7, 1, 5, 3, 6, 4]
+print(bruteForce(input_array))
+print(optimized(input_array))
 # Output = 5
