a0f39a1a3b77c95b4605d0d905a0b8d19badf714
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-19 17:18:26 -0700

    No pain

---
+++ Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/Practice.py
@@ -0,0 +1,11 @@
+# Search an array of numbers to find a target number using binary search.
+# The function should return the index of the target number if the target number is found
+# or a -1 if the target is not found in the array.
+
+
+
+input_array = [1, 2, 5, 9, 11, 12, 15, 21, 28, 99, 100, 117]
+input_target = 117
+
+print(binSearch(input_array, input_target))
+# Output = 2
