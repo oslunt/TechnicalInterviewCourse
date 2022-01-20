5b3836b2ab0eae2ff0676cdb6434de415b54bfa2
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-19 17:18:26 -0700

    No pain

--- Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
+++ Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
@@ -2,6 +2,23 @@
 # The function should return the index of the target number if the target number is found
 # or a -1 if the target is not found in the array.
 
-input_array = [1, 2, 5, 9, 12, 15, 21, 28, 99, 100, 117]
-input_target = 5
+def binSearch(input_array, input_target):
+    lower = 0
+    upper = len(input_array) - 1
+
+    while lower <= upper:
+        half = int((lower + upper)/2)
+        if input_array[half] == input_target:
+            return half
+        elif input_array[half] > input_target:
+            upper = half - 1
+        else:
+            lower = half + 1
+    
+    return -1
+
+input_array = [1, 2, 5, 9, 11, 12, 15, 21, 28, 99, 100, 117]
+input_target = 117
+
+print(binSearch(input_array, input_target))
 # Output = 2
