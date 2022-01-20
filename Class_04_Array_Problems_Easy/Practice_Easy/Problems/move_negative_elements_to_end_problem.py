2251c2519a2ae820d6242f0fd087707a5247d60c
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-19 17:18:26 -0700

    No pain

--- Class_04_Array_Problems_Easy/Practice_Easy/Problems/move_negative_elements_to_end_problem.py
+++ Class_04_Array_Problems_Easy/Practice_Easy/Problems/move_negative_elements_to_end_problem.py
@@ -1,4 +1,31 @@
 # Write a function that moves all negative elements to the end of an array
 
+def negativeEnd(input_array):
+    positive = []
+    negative = []
+    for i in range(len(input_array)):
+        if input_array[i] < 0:
+            negative.append(input_array[i])
+        else:
+            positive.append(input_array[i])
+    
+    return positive + negative
+
+def betterNegEnd(input_array):
+    #for i in range(len(input_array)):
+    right = len(input_array) - 1
+    left = 0
+    while left < len(input_array) and left < right:
+        if input_array[left] < 0:
+            temp = input_array[left]
+            input_array[left] = input_array[right]
+            input_array[right] = temp
+            right = right - 1
+        left = left + 1
+    
+    return input_array
+
 input_array = [-1, 2, -3, 4, 5, 7]
+print(negativeEnd(input_array))
+print(betterNegEnd(input_array))
 # Output = [2, 4, 5, 7, -3, -1]
--- Class_04_Array_Problems_Easy/Practice_Easy/Problems/richest_customer_wealth_problem.py
+++ Class_04_Array_Problems_Easy/Practice_Easy/Problems/richest_customer_wealth_problem.py
@@ -4,5 +4,31 @@
 # A customer's wealth is the amount of money they have in all their bank accounts. 
 # The richest customer is the customer that has the maximum wealth.
 
+def richest(input_accounts):
+    total = 0
+    max = -1
+    for i in range(len(input_accounts)):
+        for j in range(len(input_accounts[i])):
+            total = total + input_accounts[i][j]
+        if total > max:
+            max = total
+        total = 0
+    
+    return max
+
+def better_rich(input_accounts):
+    length = len(input_accounts)
+    width = len(input_accounts[0])
+    maxlength = length * width
+    total = 0
+    max = -1
+    pastrow = 0
+    for i in range(maxlength):
+        row = i/width
+        col = i%width
+        total = total + input_accounts[row][col]
+
+
 input_accounts = [[2,8,7],[7,1,3],[1,9,5]]
+print(richest(input_accounts))
 # Output: 17
