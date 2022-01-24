84d21d5b54e27130b4782355279db09b69a0c749
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-24 16:56:46 -0700

    MergeSort HW

--- Class_06_Frontend_Interviews_And_Merge_Sort/Frontend_Interviews_and_Merge_Sort_Homework/Problems/merge_sort_problem.py
+++ Class_06_Frontend_Interviews_And_Merge_Sort/Frontend_Interviews_and_Merge_Sort_Homework/Problems/merge_sort_problem.py
@@ -1,6 +1,43 @@
 # Write a merge sort algorithm to sort an array. 
 # The function should return the sorted array.
 
+def mergeSort(arr):
+    if len(arr) > 1:
+        half = len(arr) // 2
+        left = arr[:half]
+        right = arr[half:]
+        
+        mergeSort(left)
+        mergeSort(right)
+        
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1   
+
 # two examples
 array1 = [45, 98, 3, 24, 15, 77, 9, 50] # output: [3, 9, 15, 24, 45, 50, 77, 98]
 array2 = [18, 16, 27, 4, 12] # output: [4, 12, 16, 18, 27]
+
+mergeSort(array1)
+print(array1)
+
+mergeSort(array2)
+print(array2)
