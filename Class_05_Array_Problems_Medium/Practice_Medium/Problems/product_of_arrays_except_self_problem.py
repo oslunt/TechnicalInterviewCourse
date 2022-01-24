6383b2d8f225e068eb638621af0cc79ec6ce25d9
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-24 16:56:46 -0700

    MergeSort HW

--- Class_05_Array_Problems_Medium/Practice_Medium/Problems/product_of_arrays_except_self_problem.py
+++ Class_05_Array_Problems_Medium/Practice_Medium/Problems/product_of_arrays_except_self_problem.py
@@ -3,5 +3,33 @@
 # Note: Please solve it without division
 # This problem came from leetcode.com
 
+from audioop import mul
+
+
+def bruteMult(input_array):
+    prod = 1
+    lst = []
+    for i in range(len(input_array)):
+        for j in range(len(input_array)):
+            if i != j:
+                prod = input_array[j] * prod
+        lst.append(prod)
+        prod = 1
+    
+    return lst
+
+def optMulti(input_array):
+    prod = 1
+    for i in range(len(input_array)):
+        prod = prod * input_array[i]
+    lst = []
+    for i in range(len(input_array)):
+        lst.append(prod // input_array[i])
+    
+    return lst
+
 input_array = [1, 2, 3, 4]
+print(bruteMult(input_array))
+print(optMulti(input_array))
+
 # Output = [24, 12, 8, 6]
