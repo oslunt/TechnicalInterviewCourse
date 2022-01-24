dbb67f1bc9d3ad3571aeaa025d12c48bfd28e176
oslunt <77417770+oslunt@users.noreply.github.com>
2022-01-24 16:56:46 -0700

    MergeSort HW

--- Class_05_Array_Problems_Medium/Practice_Medium/Problems/minimum_rotated_sorted_array_problem.py
+++ Class_05_Array_Problems_Medium/Practice_Medium/Problems/minimum_rotated_sorted_array_problem.py
@@ -2,5 +2,22 @@
 # to you beforehand. You may assume no duplicate exists in the array.
 # This problem came from leetcode.com
 
-input_array = [3, 4, 5, 6, 1, 2]
+def findMin(input_array):
+    left = 0
+    right = len(input_array) - 1
+    while left <= right:
+        half = (right + left)//2
+        if input_array[half] < input_array[left] and input_array[half] < input_array[right]:
+            return input_array[half]
+        elif input_array[half] < input_array[left]:
+            right = half - 1
+        else:
+            left = half + 1
+    if input_array[len(input_array) - 1] < input_array[0]:
+        return input_array[len(input_array) - 1]
+    else:
+        return input_array[0]
+
+input_array = [5, 6, 1, 2, 3, 4,]
+print(findMin(input_array))
 # Output = 1
