ddb80ce1493ab2c987ddd6e46a60c2fcee879290
oslunt <77417770+oslunt@users.noreply.github.com>
2022-02-08 12:34:15 -0700

    HW Class10

--- Class_10_Linked_List_Problems_Easy/ClassPractice/Problems/python_linked_list_problem.py
+++ Class_10_Linked_List_Problems_Easy/ClassPractice/Problems/python_linked_list_problem.py
@@ -1,1 +1,44 @@
 # Create a linked list using python
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert(self, data):
+        newNode = Node(data)
+        if(self.head):
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+    
+    def print(self):
+        current = self.head
+        while(current):
+            print(current.data)
+            current = current.next
+    
+    def reverse(self):
+        current = self.head
+        prev = None
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
+
+LL = LinkedList()
+LL.insert(3)
+LL.insert(4)
+LL.insert(5)
+LL.reverse()
+LL.print()
